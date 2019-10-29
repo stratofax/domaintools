c541112f1c6e64e21b9466920d2caf25cdbefcef
Neil Johnson <neil@cadent.com>
2019-10-29 18:02:59 -0400

    Updated variable names

--- domaincheck.py
+++ domaincheck.py
@@ -7,37 +7,35 @@ import click
 import whois
 
 @click.command()
-@click.argument('host_names', nargs=-1)
+@click.argument('hostnames', nargs=-1)
 @click.option(
-    '--host_names_file',
+    '--hostsfile',
     type=click.File('r'),
     required=False,
 )
 
-def check_host_names(host_names_file, host_names):
+def check_host_names(hostsfile, hostnames):
     """
          Check IP addresses, domain registration, nameservers, and zone records
     """
-    if host_names:
+    if hostnames:
         add_s = ''
-        host_count = len(host_names)
+        host_count = len(hostnames)
         if host_count > 1: add_s = 's'
         args_listed = ('Hostname{} specified: {}'.format(add_s, host_count))
         click.echo(args_listed)
-        get_host_data(host_names)
-
-    elif host_names_file:
-        hosts_in_file = host_names_file.readlines()
+        get_host_data(hostnames)
+    elif hostsfile:
+        hosts_in_file = hostsfile.readlines()
         hosts_in_file = [x.strip() for x in hosts_in_file]
-        get_host_data(hosts_in_file)
         # click.echo(hosts_in_file)
-
+        get_host_data(hosts_in_file)
     else:          # no hostnamespassed as argument or filename
         click.echo('Please provide at least one domain name to check!')
 
-def get_host_data(hostnames):
+def get_host_data(host_list):
     host_data = ''
-    for i in hostnames:
+    for i in host_list:
         ip_addr = socket.gethostbyname(i)
         whois_data = whois.whois(i)
         # click.echo(whois_data)
